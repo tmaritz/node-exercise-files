e9e9296d2314a08ff4972b2c77225871a14a8761
tmaritz <33519371+tmaritz@users.noreply.github.com>
2020-10-28 15:55:04 +0000

    Add readable file streams

--- ch06/06_01/start/readStream.js
+++ ch06/06_01/start/readStream.js
@@ -1,4 +1,20 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
+
+let fileText = "";
+
 console.log("type something...");
-process.stdin.on("data", data => {
+readStream.on("data", data => {
   console.log(`I read ${data.length - 1} characters of text`);
+  fileText += data;
+});
+
+readStream.once("data", data => {
+  console.log(data);
+});
+
+readStream.on("end", () => {
+  console.log("finished reading");
+  console.log(`In total I read ${fileText.length -1} characters`);
 });
