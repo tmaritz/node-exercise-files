a96755e40868e21cbf747e4a0116642b87cde9fe
tmaritz <33519371+tmaritz@users.noreply.github.com>
2020-10-28 15:40:52 +0000

    Add file system basics

--- ch05/05_02/start/readfile.js
+++ ch05/05_02/start/readfile.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 fs.readFile("./assets/alex.jpg", (err, img) => {
     if (err) {
-        console.log(`An error has occured: ${err.message}`);
+        console.log(`An error has occurred: ${err.message}`);
         process.exit();
     }
 console.log("file content read");
